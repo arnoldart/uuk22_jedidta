5f67182567329d1216d4e2d0983ac50ca3bc4706
arnoldart <adoni.091203@gmail.com>
2022-04-14 11:02:37 +0700

    test

--- src/admin/utils/tambah_kamar.php
+++ src/admin/utils/tambah_kamar.php
@@ -14,14 +14,14 @@ if($foto !== "") {
   $nama_gambar_baru = $angka_acak.'-'.$foto;
 
   if(in_array($extensi, $ekstensi_diperbolehkan) === true) {
-    move_uploaded_file($file_tmp, '../../assets/img'.$nama_gambar_baru);
+    move_uploaded_file($file_tmp, 'gambar/'.$nama_gambar_baru);
     $query = "INSERT INTO kamar(no_kamar, foto) VALUES ('$no_kamar', '$nama_gambar_baru')";
     $result = mysqli_query($conn, $query);
 
     if(!$result) {
       die("query gagal dijalankan: ".mysqli_errno($conn)."-".mysqli_error($conn));
     }else {
-      echo "<script>alert('Data berhasil ditambahkan.');</script>";
+      echo "<script>alert('Data berhasil ditambahkan.');window.location='../kamar.php';</script>";
     }
   }else {
     echo "<script>alert('Extensi gambar harus png atau jpg.');window.locations='kamar.php';</script>";
