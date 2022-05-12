e201e9e1a93e0190d204c5d90d678cc8acbf93b9
arnoldart <adoni.091203@gmail.com>
2022-05-12 10:30:48 +0700

    menghadehhhhhhhh

--- reservasi_jedidta.sql
+++ reservasi_jedidta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2022 at 08:06 PM
+-- Generation Time: May 12, 2022 at 05:26 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_ukk_2022`
+-- Database: `reservasi_jedidta`
 --
 
 -- --------------------------------------------------------
@@ -73,7 +73,7 @@ CREATE TABLE `tb_kamar` (
   `jumlah` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
   `gambar` varchar(50) NOT NULL,
-  `keterangan` varchar(50) NOT NULL
+  `keterangan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -81,8 +81,11 @@ CREATE TABLE `tb_kamar` (
 --
 
 INSERT INTO `tb_kamar` (`id_kamar`, `tipe`, `jumlah`, `harga`, `gambar`, `keterangan`) VALUES
-(10, '', 23, 500000, '986-hotel-room1.jpg', 'Full AC'),
-(15, '', 2, 500000, '162-hotel-room2.jpg', 'Full AC');
+(10, 'Standart', 23, 500000, '1-hotel-room3.jpg', 'Full AC,  Kasur 1, WIFI dan Kamar mandi'),
+(15, 'Deluxe', 2, 500000, '273-hotel-room5.jpg', 'Full AC,  Kasur 1 tetapi lebar, WIFI dan kamar mandi'),
+(16, 'Superior', 23, 500000, '671-hotel-room4.jpg', 'Full AC,  Kasur 1 tetapi lebar, WIFI dan kamar mandi'),
+(17, 'Suit', 23, 1000000, '331-hotel-room1.jpg', 'Full AC,  Kasur 1 tetapi lebar, WIFI dan kamar mandi'),
+(18, 'Presidensial', 23, 1000000, '730-hotel-room2.jpg', 'Full AC,  Kasur 1 tetapi lebar, WIFI dan kamar mandi');
 
 -- --------------------------------------------------------
 
@@ -96,7 +99,7 @@ CREATE TABLE `tb_pengguna` (
   `username` varchar(50) NOT NULL,
   `password` varchar(20) NOT NULL,
   `level` enum('resepsionis','admin','tamu','') NOT NULL,
-  `no_hp` int(13) NOT NULL
+  `no_hp` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -104,8 +107,9 @@ CREATE TABLE `tb_pengguna` (
 --
 
 INSERT INTO `tb_pengguna` (`id_pengguna`, `nama_pengguna`, `username`, `password`, `level`, `no_hp`) VALUES
-(4, 'jedidta', 'jedidta', '123123', 'resepsionis', 1),
-(6, 'menghadeh', 'menghadeh', '123123', 'admin', 344);
+(4, 'jedidta', 'jedidta', '123123', 'resepsionis', '081232356789'),
+(7, 'adoni', 'adoni', '123123', 'admin', '081232356788'),
+(13, 'saputra', 'saputra', '123123', 'tamu', '081232356710');
 
 -- --------------------------------------------------------
 
@@ -120,7 +124,7 @@ CREATE TABLE `tb_tamu` (
   `password` varchar(10) NOT NULL,
   `jen_kel` enum('P','L') NOT NULL,
   `email` varchar(35) NOT NULL,
-  `no_hp` int(15) NOT NULL
+  `no_hp` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -128,10 +132,7 @@ CREATE TABLE `tb_tamu` (
 --
 
 INSERT INTO `tb_tamu` (`id`, `nama_tamu`, `username`, `password`, `jen_kel`, `email`, `no_hp`) VALUES
-(1, 'user', 'user', 'user', 'P', 'user', 8888),
-(2, '', '', '', '', '', 0),
-(4, 'hadeh', 'hadeh', 'hadeh', 'L', 'adoni.091203@gmail.com', 2147483647),
-(5, 'jedidta adoni', 'jedidta', '123123', 'L', 'adoni.091203@gmail.com', 2147483647);
+(13, 'saputra', 'saputra', '123123', 'L', 'saputra.091203@gmail.com', '081232356710');
 
 -- --------------------------------------------------------
 
@@ -154,13 +155,8 @@ CREATE TABLE `transaksi` (
 --
 
 INSERT INTO `transaksi` (`id_pesanan`, `tgl_check_in`, `tgl_check_out`, `id_tamu`, `id_kamar`, `jumlah_kamar`, `status`) VALUES
-(5, '2022-05-10', '2022-05-10', 1, 15, 23, '2'),
-(6, '2022-05-10', '2022-05-10', 1, 10, 2, '2'),
-(7, '2022-05-10', '2022-05-10', 4, 10, 2, '2'),
-(8, '2022-05-10', '2022-05-12', 5, 10, 1, '2'),
-(9, '2022-05-11', '2022-06-10', 1, 10, 2, '2'),
-(10, '2022-05-11', '2022-05-11', 1, 10, 23, '2'),
-(11, '2022-05-11', '2022-05-12', 1, 10, 23, '2');
+(12, '2022-05-12', '2022-05-13', 13, 10, 1, '2'),
+(13, '2022-05-12', '2022-05-14', 13, 17, 23, '1');
 
 --
 -- Indexes for dumped tables
@@ -222,25 +218,25 @@ ALTER TABLE `tb_fasilitas_umum`
 -- AUTO_INCREMENT for table `tb_kamar`
 --
 ALTER TABLE `tb_kamar`
-  MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `tb_pengguna`
 --
 ALTER TABLE `tb_pengguna`
-  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tb_tamu`
 --
 ALTER TABLE `tb_tamu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
